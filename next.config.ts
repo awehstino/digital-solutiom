7768e047fbd0a082d1cc5128e3c93b3764d38958
Kuro fuji <Augusthne123@gmail.com>
2026-01-24 21:37:42 +0100

    i remove the our future, bnefit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   trailingSlash: true,
+  distDir: "build",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/components/AboutUs/WhyChooseUs.tsx
+++ src/components/AboutUs/WhyChooseUs.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 const WhyChooseUs: React.FC = () => {
   // Dynamic data object
   const whyChooseData = {
+    title: "Why Choose Us",
     subtitle: "WHY CHOOSE erprises For Scalable, Automated Cyber Defense",
     description:
       "We believe cybersecurity should be smart, seamless, and scalable Our AI-powered platform continuously analyzes data",
